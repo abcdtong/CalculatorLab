2a15c40acb511b502223a7ce5ad4426a1feec0b0
isne529 <tong11_tong@hotmail.com>
2018-08-24 13:36:32 +0700

    baitong

--- CPE200Lab1/CPE200Lab1/Form1.cs
+++ CPE200Lab1/CPE200Lab1/Form1.cs
@@ -16,5 +16,55 @@ namespace CPE200Lab1
         {
             InitializeComponent();
         }
+        private void lblDisplay_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            lblDisplay.Text = "1";
+            
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            lblDisplay.Text = "2";
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            lblDisplay.Text = "3";
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            lblDisplay.Text = "4";
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            lblDisplay.Text = "6";
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            lblDisplay.Text = "7";
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            lblDisplay.Text = "8";
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            lblDisplay.Text = "9";
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            lblDisplay.Text = "9";
+        }
     }
 }
